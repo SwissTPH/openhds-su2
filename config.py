5f57c25359bf722e9586b69330bf0ff9880e2398
Nicolas Maire <nicolas.maire@unibas.ch>
2014-09-19 17:26:12 +0200

    added sample config

---
+++ config.py
@@ -0,0 +1,14 @@
+CSRF_ENABLED = True
+SECRET_KEY = 'r4i78fnwrfs7horo87v783ohg18eph0dfjfaohfr'
+
+SITE = 'Sample'
+FORM_DEFINITIONS = 'sample.json'
+ODK_DB = 'odk_prod'
+OPEN_HDS_DB = 'openhds'
+DB_HOST = 'data-management.local'
+DB_USER = 'data'
+DB_PW = 'data'
+USER = 'us'
+PWD = 'pw'
+MAX_REVISITS = '20'
+REVISIT_RADIUS = '3'
